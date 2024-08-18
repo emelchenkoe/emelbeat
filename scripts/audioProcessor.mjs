91dc6492a1469912768e0cd888320f2c34df2d99
GFLJS2100-user <reyedtreefrog@gmail.com>
2024-08-18 19:39:10 +0800

    Update audioProcessor.mjs

--- scripts/audioProcessor.mjs
+++ scripts/audioProcessor.mjs
@@ -296,6 +296,13 @@ class audioProcessor extends AudioWorkletProcessor {
 					this.getValues = (funcValue) => ((Math.cos(funcValue * Math.PI / 128) * 32)) / 127.5 - 1;
 					this.getValuesVisualizer = (funcValue) => (((Math.cos(funcValue / (128 / Math.PI) + 0.01) * 128) & 255) + 127);
 					break;
+				case 'Funcbeat':
+					this.getValues = (funcValue, ch) => {
+						const outValue = Math.max(Math.min(funcValue, 1), -1);
+						this.lastByteValue[ch] = Math.round((outValue + 1) * 127.5);
+						return outValue;
+					};
+					break;
 
 				default: this.getValues = (_funcValue) => NaN;
 					this.getValues = (_funcValue) => 0;
